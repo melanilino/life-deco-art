c691343be09486fc94a4e0f33dccc55c57dbaeb6
melanilino <chnllino@gmail.com>
2026-08-10 22:31:52 -0400

    LDA 00

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyA_Fesjo1OapCVS83_8uycSms1QPox02Lw",
+  authDomain: "lifedecoart-cms.firebaseapp.com",
+  projectId: "lifedecoart-cms",
+  storageBucket: "lifedecoart-cms.firebasestorage.app",
+  messagingSenderId: "122525582620",
+  appId: "1:122525582620:web:318dd66d9e1c25bb9126d9",
+  measurementId: "G-RBHTSXYSWW"
+};
